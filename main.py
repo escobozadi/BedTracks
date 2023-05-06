48eefd571d0d99e5cd6c201bd3265a34d15568b5
escobozadi <dianaescoboza@gmail.com>
2023-05-06 13:21:32 -0400

    SOCAN .dat into pandas table

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+"""
+Code to modify SOCAN Publisher File into a table according to the layout
+"""
+import csv
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+import numpy as np
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def create_connection(host_name, user_name, password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=password
+        )
+        print("Connection successful!")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database creation successful!")
+    except Error as err:
+        print(f"Error: '{err}'")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # create_connection("localhost", "root", "Dakota.2486Nala")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # datFile = pd.read_table("B01_SOCAN_2012toJun2022.dat")
+    datFile = pd.read_fwf("B01_SOCAN_2012toJun2022.dat", header=None, index_col=False,
+                          colspecs=[(0, 4), (4, 5), (5, 50), (50, 60), (60, 90), (90, 113), (113, 117)],
+                          names=["DIST-YEAR", "DIST-QTR", "SUPPLIER-NAME", "SONG-CODE", "SONG-TITLE", "SONG-WRITER",
+                                 "INCOME-TYPE"])
+    print(datFile)
