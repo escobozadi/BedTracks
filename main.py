15c35c1d6d2009306cfc6c09c3628d4dbe880c3b
escobozadi <dianaescoboza@gmail.com>
2023-05-07 15:43:06 -0400

    cleaning csv's into df's

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Code to modify SOCAN Publisher File into a table according to the layout
+Code to analyze SOCAN Royalty statements
 """
 import csv
 import mysql.connector
@@ -9,28 +9,55 @@ import numpy as np
 from tabulate import tabulate
 
 
-def create_connection(host_name, user_name, password):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=password
-        )
-        print("Connection successful!")
-    except Error as err:
-        print(f"Error: '{err}'")
+class SQLConnect(object):
 
-    return connection
+    def __init__(self, host_name, user_name, passw):
+        self.host_name = host_name
+        self.user_name = user_name
+        self.password = passw
+        self.connection = None
 
+    def create_connection(self):
+        self.connection = None
+        try:
+            self.connection = mysql.connector.connect(
+                host=self.host_name,
+                user=self.user_name,
+                passwd=self.password
+            )
+            print("Connection successful!")
+        except Error as err:
+            print(f"Error: '{err}'")
 
-def create_database(connection, query):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        print("Database creation successful!")
-    except Error as err:
-        print(f"Error: '{err}'")
+    def create_database(self, query):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(query)
+            print("Database creation successful!")
+        except Error as err:
+            print(f"Error: '{err}'")
+
+    def connect_db(self, db_name):
+        self.connection = None
+        try:
+            self.connection = mysql.connector.connect(
+                host=self.host_name,
+                user=self.user_name,
+                passwd=self.password,
+                database=db_name
+            )
+            print("Databse Connection Successful!")
+        except Error as err:
+            print(f"Error: '{err}'")
+
+    def execute_query(self, query):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(query)
+            self.connection.commit()
+            print("Query successful!")
+        except Error as err:
+            print(f"Error: '{err}'")
 
 
 def datFormat():
@@ -51,17 +78,62 @@ def datFormat():
                                  "PERF-DATE", "PERFORMERS", "ISWC", "STATION", "VENUE",
                                  "CITY", "PERF-TYPE"])
     dataFrame = pd.DataFrame(data=datFile)
-    pd.set_option("display.max_columns", len(dataFrame))
-    print(dataFrame)
-    pd.reset_option("display.max_columns")
-
+    # pd.set_option("display.max_columns", len(dataFrame))
+    # print(dataFrame)
+    # pd.reset_option("display.max_columns")
     dataFrame.to_csv("./dat_csv.csv", encoding="utf-8", index=False)
 
 
-if __name__ == '__main__':
-    # create_connection("localhost", "root", "Dakota.2486Nala")
-    datFormat()
+def cleanDataFrames():
+    df1 = pd.DataFrame(pd.read_csv("./dat_csv.csv", index_col=False))
+    df2 = pd.DataFrame(pd.read_csv("./B01_SOCAN_2012toJun2022.csv", index_col=False))
 
-    # print(tabulate(dataFrame, headers="keys", tablefmt="psql"))
-    # display = pd.options.display
-    # display(dataFrame)
+    # DataFrame #1
+    df1["DIST-PERIOD-START"] = pd.to_datetime(df1["DIST-PERIOD-START"], format="%Y%m%d")
+    df1["DIST-PERIOD-END"] = pd.to_datetime(df1["DIST-PERIOD-END"], format="%Y%m%d")
+
+    # DataFrame #2
+    df2 = df2.rename(columns={" Work Number": "Work Number"})
+    df2["Work Number"] = pd.to_numeric(df2["Work Number"])
+
+    df2["Distribution Date"] = pd.to_datetime(df2["Distribution Date"])
+    df2["Dist. Period Start"] = pd.to_datetime(df2["Dist. Period Start"])
+    df2["Dist. Period End"] = pd.to_datetime(df2["Dist. Period End"])
+
+    return
+
+
+if __name__ == '__main__':
+    # datFormat()
+    # sql = SQLConnect("localhost", "root", "Dakota.2486Nala")
+    # sql.create_connection()
+    # create_database_query = "CREATE DATABASE Records"
+    # sql.create_database(create_database_query)
+
+    df1 = pd.DataFrame(pd.read_csv("./dat_csv.csv", index_col=False))
+    df2 = pd.DataFrame(pd.read_csv("./B01_SOCAN_2012toJun2022.csv", index_col=False))
+    pd.set_option("display.max_columns", len(df1))
+
+    print(df1.dtypes)
+    print(df1)
+
+    print(df2.dtypes)
+    print(df2)
+
+    df2 = df2.rename(columns={" Work Number": "Work Number"})
+
+    df1["DIST-PERIOD-START"] = pd.to_datetime(df1["DIST-PERIOD-START"], format="%Y%m%d")
+    df1["DIST-PERIOD-END"] = pd.to_datetime(df1["DIST-PERIOD-END"], format="%Y%m%d")
+    print(df1["DIST-PERIOD-START"])
+    df2["Distribution Date"] = pd.to_datetime(df2["Distribution Date"])
+    df2["Dist. Period Start"] = pd.to_datetime(df2["Dist. Period Start"])
+    df2["Dist. Period End"] = pd.to_datetime(df2["Dist. Period End"])
+
+    df2["Work Number"] = pd.to_numeric(df2["Work Number"])
+
+    df3 = pd.merge(df1, df2, left_on=["SONG-CODE"],
+                   right_on=["Work Number"], how='left')
+    print(df3)
+
+    pd.reset_option("display.max_columns")
+
